34885b459f65d1ccc0a8263dffd5c7ef6c4f3e99
aulamovil06 <aulamovil06@UBAULAM06-28.salas.edu.co>
2018-10-01 10:55:07 -0500

    Primera version de GUI

---
+++ GUI/src/vista/InterfazGui.java
@@ -0,0 +1,20 @@
+package vista;
+
+import javax.swing.JFrame;
+
+public class InterfazGui extends JFrame 
+{
+	public InterfazGui()
+	{
+		setSize(400, 400);
+		setResizable(false);
+		setTitle("Conversión");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocation(0, 0);
+		
+		
+		
+	}
+	
+
+}
